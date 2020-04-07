0b56afbc55dba22346919a3a2a3a991bde4db2c7
Mario Ochoa <mario8ato@hotmail.com>
2020-04-06 19:18:23 -0500

    Rutas solo para admins [ADD] middleware authAdmin findUser

--- routes/aplications.js
+++ routes/aplications.js
@@ -1,19 +1,19 @@
 const express = require('express');
 let router = express.Router();
 
-// const authenticateAdmin = require('../middlewares/authenticateAdmin');
+const authenticateAdmin = require('../middlewares/authenticateAdmin');
+const findUser = require('../middlewares/findUser');
 
-const authenticateOwner = require('../middlewares/authOwner');
-// const findUser = require('../middlewares/findUser');
+// const authenticateOwner = require('../middlewares/authOwner');
 const aplicationsController = require('../controllers/AplicationsController');
 
 const jwtMiddleware = require('express-jwt');
 const secrets = require('../config/secret');
 
-// router.all('*',jwtMiddleware({secret: secrets.jwtSecret}),findUser,authenticateAdmin)
+router.all('*',jwtMiddleware({secret: secrets.jwtSecret}),findUser,authenticateAdmin)
 
 router.route('/')
-    .get(jwtMiddleware({secret: secrets.jwtSecret}),aplicationsController.index)
+    .get(aplicationsController.index)
     .post(aplicationsController.create);
 
 router.route('/:id')
